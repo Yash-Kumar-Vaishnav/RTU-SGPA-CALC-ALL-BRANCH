5ff33a5fe0add9a8c4c5aefb80c9225c1071219f
YashVaishnav19 <yashkumarvaishnav.ece26@jecrc.ac.in>
2025-04-24 23:04:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,107 @@
 document.addEventListener("DOMContentLoaded", function () {
   const subjectsData = {
-  CSE: {
-   "1st Semester": [
-    { name: "Mathematics-I", credits: 4 },
-    { name: "Engineering Physics", credits: 4 },
+  CSE = {
+  "1st Semester": [
+    { name: "Engineering Mathematics-I", credits: 4 },
     { name: "Engineering Chemistry", credits: 4 },
-    { name: "Basic Electrical Engineering", credits: 3 },
-    { name: "Fundamentals of Mechanical Engineering", credits: 3 },
-    { name: "Programming for Problem Solving", credits: 3 },
-    { name: "Engineering Physics Lab", credits: 1 },
+    { name: "Human Values", credits: 2 },
+    { name: "Programming for Problem Solving", credits: 2 },
+    { name: "Basic Civil Engineering", credits: 2 },
     { name: "Engineering Chemistry Lab", credits: 1 },
-    { name: "Basic Electrical Engineering Lab", credits: 1 },
-    { name: "Programming Lab", credits: 1 },
-    { name: "Communication Skills Lab", credits: 1 }
+    { name: "Human Values Activities", credits: 1 },
+    { name: "Computer Programming Lab", credits: 1.5 },
+    { name: "Basic Civil Engineering Lab", credits: 1 },
+    { name: "Computer Aided Engineering Graphics", credits: 1.5 },
+    { name: "DECA", credits: 0.5 }
   ],
   "2nd Semester": [
-    { name: "Mathematics-II", credits: 4 },
-    { name: "Engineering Mechanics", credits: 3 },
-    { name: "Engineering Graphics", credits: 3 },
-    { name: "Basic Civil Engineering", credits: 3 },
-    { name: "Environment and Ecology", credits: 2 },
-    { name: "Data Structures using C", credits: 3 },
-    { name: "Engineering Mechanics Lab", credits: 1 },
-    { name: "Engineering Graphics Lab", credits: 1 },
-    { name: "Civil Engineering Lab", credits: 1 },
-    { name: "Data Structures Lab", credits: 1 },
-    { name: "Soft Skills Lab", credits: 1 }
+    { name: "Engineering Mathematics-II", credits: 4 },
+    { name: "Engineering Physics", credits: 4 },
+    { name: "Communication Skills", credits: 2 },
+    { name: "Basic Mechanical Engineering", credits: 2 },
+    { name: "Basic Electrical Engineering", credits: 2 },
+    { name: "Engineering Physics Lab", credits: 1 },
+    { name: "Language Lab", credits: 1 },
+    { name: "Manufacturing Practices Workshop", credits: 1.5 },
+    { name: "Basic Electrical Engineering Lab", credits: 1 },
+    { name: "Computer Aided Machine Drawing", credits: 1.5 },
+    { name: "DECA", credits: 0.5 }
   ],
   "3rd Semester": [
-    { name: "Discrete Mathematics", credits: 4 },
-    { name: "Digital Electronics", credits: 4 },
-    { name: "Data Structures and Algorithms", credits: 4 },
-    { name: "Object Oriented Programming using C++", credits: 4 },
-    { name: "Computer Organization and Architecture", credits: 4 },
-    { name: "Digital Electronics Lab", credits: 1 },
-    { name: "DSA Lab", credits: 1 },
-    { name: "OOPs Lab", credits: 1 },
-    { name: "Mini Project / Seminar / Internship", credits: 1 }
+    { name: "Advanced Engineering Mathematics", credits: 3 },
+    { name: "Managerial Economics & Financial Accounting", credits: 2 },
+    { name: "Digital Electronics", credits: 3 },
+    { name: "Data Structures and Algorithms", credits: 3 },
+    { name: "Object Oriented Programming", credits: 3 },
+    { name: "Software Engineering", credits: 3 },
+    { name: "Data Structures and Algorithms Lab", credits: 1.5 },
+    { name: "Object Oriented Programming Lab", credits: 1.5 },
+    { name: "Software Engineering Lab", credits: 1.5 },
+    { name: "Digital Electronics Lab", credits: 1.5 },
+    { name: "Industrial Training", credits: 1 },
+    { name: "Social Outreach, Discipline & Extracurricular Activities", credits: 0.5 }
   ],
   "4th Semester": [
-    { name: "Operating Systems", credits: 4 },
-    { name: "Database Management Systems", credits: 4 },
-    { name: "Theory of Computation", credits: 4 },
-    { name: "Software Engineering", credits: 3 },
-    { name: "Microprocessor & Interfacing", credits: 3 },
-    { name: "DBMS Lab", credits: 1 },
-    { name: "Operating Systems Lab", credits: 1 },
-    { name: "Microprocessor Lab", credits: 1 },
-    { name: "Seminar / Workshop", credits: 1 }
+    { name: "Discrete Mathematics Structure", credits: 3 },
+    { name: "Technical Communication", credits: 2 },
+    { name: "Microprocessor & Interfaces", credits: 3 },
+    { name: "Database Management System", credits: 3 },
+    { name: "Theory of Computation", credits: 3 },
+    { name: "Data Communication and Computer Networks", credits: 3 },
+    { name: "Microprocessor & Interfaces Lab", credits: 1 },
+    { name: "Database Management System Lab", credits: 1.5 },
+    { name: "Network Programming Lab", credits: 1.5 },
+    { name: "Linux Shell Programming Lab", credits: 1 },
+    { name: "Java Lab", credits: 1 },
+    { name: "Social Outreach, Discipline & Extracurricular Activities", credits: 0.5 }
   ],
   "5th Semester": [
-    { name: "Design and Analysis of Algorithms", credits: 4 },
-    { name: "Web Technology", credits: 3 },
-    { name: "Computer Networks", credits: 4 },
-    { name: "Compiler Design", credits: 4 },
-    { name: "Artificial Intelligence", credits: 3 },
-    { name: "DAA Lab", credits: 1 },
-    { name: "Web Tech Lab", credits: 1 },
-    { name: "CN Lab", credits: 1 },
-    { name: "Internship / Mini Project", credits: 1 }
+    { name: "Information Theory & Coding", credits: 2 },
+    { name: "Compiler Design", credits: 3 },
+    { name: "Operating System", credits: 3 },
+    { name: "Computer Graphics & Multimedia", credits: 3 },
+    { name: "Analysis of Algorithms", credits: 3 },
+    { name: "Human-Computer Interaction", credits: 2 },
+    { name: "Computer Graphics & Multimedia Lab", credits: 1 },
+    { name: "Compiler Design Lab", credits: 1 },
+    { name: "Analysis of Algorithms Lab", credits: 1 },
+    { name: "Advance Java Lab", credits: 1 },
+    { name: "Industrial Training", credits: 2.5 },
+    { name: "Social Outreach, Discipline & Extracurricular Activities", credits: 0.5 }
   ],
   "6th Semester": [
-    { name: "Machine Learning", credits: 4 },
+    { name: "Digital Image Processing", credits: 2 },
+    { name: "Machine Learning", credits: 3 },
+    { name: "Information Security Systems", credits: 2 },
+    { name: "Computer Architecture and Organization", credits: 3 },
+    { name: "Artificial Intelligence", credits: 2 },
     { name: "Cloud Computing", credits: 3 },
-    { name: "Internet of Things (IoT)", credits: 3 },
-    { name: "Cryptography & Network Security", credits: 4 },
-    { name: "Elective - I", credits: 3 },
-    { name: "ML Lab", credits: 1 },
-    { name: "IoT Lab", credits: 1 },
-    { name: "CNS Lab", credits: 1 },
-    { name: "Project Phase - I", credits: 2 }
+    { name: "Distributed Systems", credits: 2 },
+    { name: "Digital Image Processing Lab", credits: 1.5 },
+    { name: "Machine Learning Lab", credits: 1.5 },
+    { name: "Python Lab", credits: 1.5 },
+    { name: "Mobile App Development Lab", credits: 1.5 },
+    { name: "Social Outreach, Discipline & Extracurricular Activities", credits: 0.5 }
   ],
   "7th Semester": [
-    { name: "Data Science", credits: 3 },
-    { name: "Blockchain Technology", credits: 3 },
-    { name: "DevOps", credits: 3 },
-    { name: "Elective - II", credits: 3 },
-    { name: "Project Phase - II", credits: 4 },
-    { name: "Seminar / Internship", credits: 2 }
+    { name: "Environmental Engineering & Disaster Management", credits: 3 },
+    { name: "Internet Of Things", credits: 3 },
+    { name: "Internet Of Things Lab", credits: 2 },
+    { name: "Cyber Security Lab", credits: 2 },
+    { name: "Industrial Training", credits: 2.5 },
+    { name: "Seminar", credits: 2 },
+    { name: "Social Outreach, Discipline & Extracurricular Activities", credits: 0.5 }
   ],
   "8th Semester": [
-    { name: "Major Project", credits: 10 },
-    { name: "Internship / Industrial Training", credits: 4 }
+    { name: "Big Data Analytics", credits: 3 },
+    { name: "Big Data Analytics Lab", credits: 2 },
+    { name: "Software Testing & Validation Lab", credits: 1 },
+    { name: "Project", credits: 7 },
+    { name: "Social Outreach, Discipline & Extracurricular Activities", credits: 0.5 },
+    { name: "Disaster Management", credits: 3 }
   ]
 },
-  ECE : {
+ECE : {
     "1st Semester": [
       { "name": "Engineering Mathematics-I", "credits": 4 },
       { "name": "Engineering Physics", "credits": 4 },
@@ -110,77 +127,76 @@ document.addEventListener("DOMContentLoaded", function () {
       { "name": "Basic Civil Engineering Lab", "credits": 1 },
       { "name": "Computer Aided Machine Drawing", "credits": 1.5 }
     ],
-    "3rd Semester": [
-      { "name": "Mathematics-III", "credits": 4 },
-      { "name": "Electronic Devices & Circuits", "credits": 4 },
-      { "name": "Circuit Analysis & Synthesis", "credits": 4 },
-      { "name": "Electronic Measurements & Instrumentation", "credits": 4 },
-      { "name": "Electronic Materials & Processes", "credits": 4 },
-      { "name": "Data Structures & Algorithms", "credits": 4 },
-      { "name": "Electronics Workshop", "credits": 1 },
-      { "name": "Computer Programming Lab-I", "credits": 1 },
-      { "name": "Electronics Lab I", "credits": 1 },
-      { "name": "Electronic Measurement & Instrumentation Lab", "credits": 1 }
-    ],
-    "4th Semester": [
-      { "name": "Mathematics-IV", "credits": 4 },
-      { "name": "Analog Electronics", "credits": 4 },
-      { "name": "Digital Electronics", "credits": 4 },
-      { "name": "Electromagnetic Field Theory", "credits": 4 },
-      { "name": "Random Variables & Stochastic Processes", "credits": 4 },
-      { "name": "Object Oriented Programming", "credits": 4 },
-      { "name": "Computer Programming Lab-II", "credits": 1 },
-      { "name": "Electronics Lab II", "credits": 1 },
-      { "name": "Digital Electronics Lab", "credits": 1 },
-      { "name": "Humanities", "credits": 1 }
-    ],
-    "5th Semester": [
-      { "name": "Signals and Systems", "credits": 4 },
-      { "name": "Linear Integrated Circuits", "credits": 4 },
-      { "name": "Telecommunication Engineering", "credits": 4 },
-      { "name": "Analog Communication", "credits": 4 },
-      { "name": "Microwave Engineering-I", "credits": 4 },
-      { "name": "Biomedical Instrumentation", "credits": 4 },
-      { "name": "Electronic Engineering Design Lab", "credits": 1 },
-      { "name": "Microwave Engineering Lab", "credits": 1 },
-      { "name": "Communication Lab-I", "credits": 1 },
-      { "name": "Signal Processing Lab-I", "credits": 1 }
-    ],
-    "6th Semester": [
-      { "name": "Microwave Engineering-II", "credits": 4 },
-      { "name": "Microprocessor and Microcontroller", "credits": 4 },
-      { "name": "Industrial Electronics", "credits": 4 },
-      { "name": "Digital Communication", "credits": 4 },
-      { "name": "Control Systems", "credits": 4 },
-      { "name": "Neural Networks", "credits": 4 },
-      { "name": "Communication Lab-II", "credits": 1 },
-      { "name": "Microprocessor Lab", "credits": 1 },
-      { "name": "Unix Shell Programming Lab", "credits": 1 },
-      { "name": "Industrial Electronics Lab", "credits": 1 }
-    ],
-    "7th Semester": [
-      { "name": "Antenna & Wave Propagation", "credits": 4 },
-      { "name": "Digital Signal Processing", "credits": 4 },
-      { "name": "Wireless Communication", "credits": 4 },
-      { "name": "IC Technology", "credits": 4 },
-      { "name": "VLSI Design", "credits": 4 },
-      { "name": "Advanced Microprocessors", "credits": 4 },
-      { "name": "Signal Processing Lab-II", "credits": 1 },
-      { "name": "Wireless Communication Lab", "credits": 1 }
-    ],
-    "8th Semester": [
-      { "name": "Computer Networks", "credits": 4 },
-      { "name": "Radar & TV Engineering", "credits": 4 },
-      { "name": "Optical Communication", "credits": 4 },
-      { "name": "Image Processing and Pattern Recognition", "credits": 4 },
-      { "name": "Computer Network Programming Lab", "credits": 1 },
-      { "name": "Industrial Economics & Management", "credits": 1 },
-      { "name": "VLSI & Optical Fiber Lab", "credits": 1 }
-    ]
-  }
+   "3rd Semester": [
+    { "name": "Advanced Engineering Mathematics-I", "credits": 3 },
+    { "name": "Electronic Devices", "credits": 3 },
+    { "name": "Digital System Design", "credits": 3 },
+    { "name": "Signals and Systems", "credits": 3 },
+    { "name": "Network Theory", "credits": 3 },
+    { "name": "Electronic Devices Lab", "credits": 1.5 },
+    { "name": "Digital System Design Lab", "credits": 1.5 },
+    { "name": "Signals and Systems Lab / Simulation Lab", "credits": 1 },
+    { "name": "Social Outreach, Discipline & Extra Curricular Activities", "credits": 0.5 }
+  ],
+  "4th Semester": [
+    { "name": "Advanced Engineering Mathematics-II", "credits": 3 },
+    { "name": "Managerial Economics and Financial Accounting / Technical Communication", "credits": 2 },
+    { "name": "Analog Circuits", "credits": 3 },
+    { "name": "Microcontrollers", "credits": 3 },
+    { "name": "Electronics Measurement & Instrumentation", "credits": 3 },
+    { "name": "Analog and Digital Communication", "credits": 3 },
+    { "name": "Analog and Digital Communication Lab", "credits": 1.5 },
+    { "name": "Analog Circuits Lab", "credits": 1.5 },
+    { "name": "Microcontrollers Lab", "credits": 1.5 },
+    { "name": "Electronics Measurement & Instrumentation Lab", "credits": 1.5 },
+    { "name": "Social Outreach, Discipline & Extra Curricular Activities", "credits": 0.5 }
+  ],
+  "5th Semester": [
+    { "name": "Computer Architecture", "credits": 2 },
+    { "name": "Electromagnetic Waves", "credits": 3 },
+    { "name": "Control System", "credits": 3 },
+    { "name": "Digital Signal Processing", "credits": 3 },
+    { "name": "Microwave Theory & Techniques", "credits": 3 },
+    { "name": "Professional Elective I", "credits": 2 },
+    { "name": "RF Simulation Lab", "credits": 1.5 },
+    { "name": "Digital Signal Processing Lab", "credits": 1.5 },
+    { "name": "Microwave Lab", "credits": 1 },
+    { "name": "Industrial Training", "credits": 2.5 },
+    { "name": "Social Outreach, Discipline & Extra Curricular Activities", "credits": 0.5 }
+  ],
+  "6th Semester": [
+    { "name": "Power Electronics", "credits": 2 },
+    { "name": "Computer Network", "credits": 3 },
+    { "name": "Fiber Optics Communications", "credits": 3 },
+    { "name": "Antennas and Propagation", "credits": 3 },
+    { "name": "5G Communication Technology", "credits": 3 },
+    { "name": "Professional Elective II", "credits": 3 },
+    { "name": "Computer Network Lab", "credits": 2 },
+    { "name": "Antenna and Wave Propagation Lab", "credits": 1 },
+    { "name": "Electronics Design Lab", "credits": 2 },
+    { "name": "Power Electronics Lab", "credits": 1 },
+    { "name": "Social Outreach, Discipline & Extra Curricular Activities", "credits": 0.5 }
+  ],
+  "7th Semester": [
+    { "name": "Program Elective III", "credits": 3 },
+    { "name": "Open Elective I", "credits": 3 },
+    { "name": "VLSI Design Lab", "credits": 2 },
+    { "name": "5G Communication Lab", "credits": 1 },
+    { "name": "Optical Communication Lab", "credits": 1 },
+    { "name": "Industrial Training", "credits": 2.5 },
+    { "name": "Seminar", "credits": 2 },
+    { "name": "Social Outreach, Discipline & Extra Curricular Activities", "credits": 0.5 }
+  ],
+  "8th Semester": [
+    { "name": "Program Elective IV", "credits": 3 },
+    { "name": "Open Elective II", "credits": 3 },
+    { "name": "Internet of Things (IOT) Lab", "credits": 1 },
+    { "name": "Skill Development Lab", "credits": 1 },
+    { "name": "Major Project", "credits": 7 },
+    { "name": "Social Outreach, Discipline & Extra Curricular Activities", "credits": 0.5 }
+  ]
 };
-
-  const gradePoints = {
+ const gradePoints = {
   "A++": 10,
   "A+": 9,
   "A": 8.5,
